18e0a6908fbe1f25fe9999b42f4a8854013fb6e2
Daniel Azamar-Chacha <daniel@orenda.local>
2018-10-11 00:05:03 -0500

    Actualizando proyecto: Configuraciones listas, desarrollo listo

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div >
-        <h1>First version of the app v0.1</h1>
+        <h1>First version of the app</h1>
       </div>
     );
   }
