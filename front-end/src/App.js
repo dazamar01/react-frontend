9ae0d6a6930025356f19865495829dd9cb45b259
Daniel Azamar-Chacha <daniel@orenda.local>
2018-10-21 16:03:52 -0500

    Agregando estructura

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import Layout from './hoc/Layout/Layout';
+
 import './App.css';
 
 class App extends Component {
   render() {
+    let routes = null;
+    /*
+    routes = (
+      <Switch>
+        <Route path="/auth" component={Logout} />
+        
+        <Redirect to="/" />
+      </Switch>
+    );
+      */
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Layout>
+          
+        </Layout>
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter( App ) ;
