6c9514f1e851ea219e2824e512e58652ae53ddfd
Daniel Azamar-Chacha <daniel@orenda.local>
2018-10-21 21:09:10 -0500

    Actualizando

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,7 @@
 import React, { Component } from 'react';
-// import ReactDOM from 'react-dom';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 
 import { connect } from 'react-redux';
-
-// import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-// import thunk from 'redux-thunk';
 
 import Auth from '../src/containers/Auth/AuthContainer';
 import Layout from './hoc/Layout/Layout';
